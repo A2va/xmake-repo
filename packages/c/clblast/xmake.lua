ae532a0fee2ed7af619c2c899b326ed1c5aaf721
A2va <49582555+A2va@users.noreply.github.com>
2023-07-02 18:26:27 +0200

    Add clblast

--- packages/c/clblast/xmake.lua
+++ packages/c/clblast/xmake.lua
@@ -15,16 +15,20 @@ package("clblast")
     add_configs("netlib", { description = "Enable compilation of the CBLAS Netlib API", default = false, type = "boolean" })
     add_configs("netlib_persistent_opencl", { description = "Makes OpenCL device and context in the CBLAS Netlib API static", default = false, type = "boolean" })
 
+    if is_plat("linux") then
+        add_extsources("apt::libclblast-dev")
+    elseif is_plat("macosx") then
+        add_extsources("brew::clblast")
+    end
 
     on_load(function (package) 
         if package:config("cuda") then
             package:add("deps", "cuda")
         end
-        -- if package:config("opencl") then
-        --     package:add("deps", "opencl")
-        -- end
+        if package:config("opencl") then
+           package:add("deps", "opencl")
+        end
     end)
-
 
     on_install(function (package) 
         local configs = {}
@@ -36,6 +40,7 @@ package("clblast")
 
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
         table.insert(configs, "-DVERBOSE=" .. (package:config("verbose") and "ON" or "OFF"))
         table.insert(configs, "-DAMD_SI_EMPTY_KERNEL_WORKAROUND=" .. (package:config("workaround") and "ON" or "OFF"))
 
@@ -45,6 +50,5 @@ package("clblast")
         table.insert(configs, "-DNETLIB=" .. (package:config("cuda") and "ON" or "OFF"))
         table.insert(configs, "-DNETLIB_PERSISTENT_OPENCL=" .. (package:config("cuda") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
-
     end)
 
